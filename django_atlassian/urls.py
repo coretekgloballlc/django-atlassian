3b8b8868546a1bcf6f707a91e14dbbce18008f1e
Jay Chow <jchow@cadence-labs.com>
2022-10-12 17:36:41 -0700

    update urls to be django4+ compatible

--- django_atlassian/urls.py
+++ django_atlassian/urls.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf.urls import url
+from django.urls import re_path
 
 import views
 
 urlpatterns = [
-    url(r'^installed/$', views.installed, name='django-atlassian-installed'),
-    url(r'^jira/$', views.JiraDescriptor.as_view(), name='django-atlassian-jira-connect-json'),
-    url(r'^confluence/$', views.ConfluenceDescriptor.as_view(), name='django-atlassian-confluence-connect-json'),
+    re_path(r'^installed/$', views.installed, name='django-atlassian-installed'),
+    re_path(r'^jira/$', views.JiraDescriptor.as_view(), name='django-atlassian-jira-connect-json'),
+    re_path(r'^confluence/$', views.ConfluenceDescriptor.as_view(), name='django-atlassian-confluence-connect-json'),
 ]
